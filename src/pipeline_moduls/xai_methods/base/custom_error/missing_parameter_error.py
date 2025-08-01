9748b1f5d6cae475495367a403b265121dcbf14a
rahman <shium.rahman@adesso.de>
2025-08-01 21:44:20 +0200

    delete unsed custom errors

--- src/pipeline_moduls/xai_methods/base/custom_error/missing_parameter_error.py
+++
@@ -1,7 +0,0 @@
-from src.pipeline_moduls.xai_methods.base.custom_error.config_vaildation_error import (
-    ConfigValidationError,
-)
-
-
-class MissingParameterError(ConfigValidationError):
-    pass
