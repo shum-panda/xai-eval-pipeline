1512c001b9932e8a782172847b2364bd94251e26
rahman <shium.rahman@adesso.de>
2025-08-01 14:21:04 +0200

    multi run experiment setup

--- mulitrun_main.py
+++ mulitrun_main.py
@@ -7,21 +7,25 @@ from src.control.utils.config_dataclasses.master_config import MasterConfig
 
 
 def run_multiple_configs(config_names: list[str]) -> None:
-    with initialize(config_path="config"):
+    with initialize(version_base=None, config_path="config"):
         for config_name in config_names:
-            print(f"\n🔁 Running config: {config_name}")
-            cfg = compose(config_name=config_name)
-            print(OmegaConf.to_yaml(cfg))
+            try:
+                print(f"\n🔁 Running config: {config_name}")
+                cfg = compose(config_name=config_name)
+                print(OmegaConf.to_yaml(cfg))
 
-            pipeline = Orchestrator(cfg)  # Deine Pipeline-Instanz
-            pipeline.run()
+                pipeline = Orchestrator(cfg)  # Deine Pipeline-Instanz
+                pipeline.run()
 
-            print(f"✅ Finished run for: {config_name}")
+                print(f"✅ Finished run for: {config_name}")
+            except Exception as e:
+                print(f"while {config_name} a error accorded: {e}")
 
 
 if __name__ == "__main__":
     run_multiple_configs([
-        "config_grad_cam",
         "config_score_cam",
-        "config_guided_backprop"
+        "config_guided_backprop",
+        "config_integrated_gradient",
+        "config_grad_cam_vgg16",
     ])
