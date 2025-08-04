7c603c6141c9b41ad7d3c06c083ce294168dc638
rahman <shium.rahman@adesso.de>
2025-08-05 01:40:57 +0200

    add comment that device statment is ignored

--- src/control/utils/config_dataclasses/hardware_config.py
+++ src/control/utils/config_dataclasses/hardware_config.py
@@ -4,4 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class HardwareConfig:
     use_cuda: bool = True
-    device: str = "cuda:0"
+    device: str = "cuda:0" #currently ignored, system checks availability
